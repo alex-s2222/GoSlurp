7f985eff17f691b40c66fa0662ea9a5ddc83ef88
Juzo(-.-) <alexs22221@gmail.com>
2025-01-31 23:55:19 +0300

    .

--- protocols/go.mod
+++
@@ -1,8 +0,0 @@
-module protocols
-
-go 1.23.0
-
-require (
-	google.golang.org/protobuf v1.35.1
-	gopkg.in/yaml.v3 v3.0.1
-)
--- protocols/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "protocols/exec"
-
-func main() {
-	exec.ProtobufCase()
-	// exec.ParseJson()
-	// exec.CreateJson()
-}
