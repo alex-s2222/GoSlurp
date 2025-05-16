d9ed93b5d866b82cb7bedad45896e0b2b7983503
Juzo(-.-) <alexs22221@gmail.com>
2025-05-16 22:59:21 +0300

    docker logs

---
+++ 07Docker/02SDK/example/print_logs.go
@@ -0,0 +1,26 @@
+package example
+
+import (
+	"context"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func PrintLogs(container string){
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil{ 
+		panic(err)
+	}
+	options := types.ContainerLogsOptions{ShowStdout: true}
+
+	out, err := cli.ContainerLogs(ctx, container, options)
+	if err != nil{
+		panic(err)
+	}
+
+	io.Copy(os.Stdout, out)
+}
