dd6a356679180a156d28f203d208794949375e14
Juzo(-.-) <alexs22221@gmail.com>
2025-06-29 20:40:24 +0300

    context

---
+++ mySpace/4_context/context.go
@@ -0,0 +1,33 @@
+ package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func worker(ctx context.Context, id int) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("Worker %d stopped\n", id)
+			return
+		default:
+			fmt.Printf("Worker %d is working...\n", id)
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for i := 1; i <= 3; i++ {
+		go worker(ctx, i)
+	}
+
+	time.Sleep(2 * time.Second)
+	cancel()
+
+	time.Sleep(1 * time.Second)
+}
